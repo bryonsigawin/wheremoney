891dd9c188dc65d43001a774b9faa75653c19887
Bryon Sigawin <zh3nlong@gmail.com>
2023-06-14 18:45:07 +0800

    feat: create frame element

---
+++ src/lib/components/window.svelte
@@ -0,0 +1,13 @@
+<div class="border-2 border-solid border-black shadowed">
+	<div class="border-[3px] border-solid border-white border-r-gray-300 border-b-gray-300">
+		<div class="p-8 border-t-4 border-solid border-black">
+			<slot />
+		</div>
+	</div>
+</div>
+
+<style>
+	.shadowed {
+		box-shadow: 10px 15px 0 rgba(0 0 0 / 0.5);
+	}
+</style>
