aa4b7b5e8920510bc84197215cbe8d05b5fd74b3
Bryon Sigawin <zh3nlong@gmail.com>
2023-06-14 17:58:13 +0800

    feat: roughly setup font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,23 @@
 <script>
+	import '@fontsource/iosevka-etoile/100.css';
+	import '@fontsource/iosevka-etoile/100-italic.css';
+	import '@fontsource/iosevka-etoile/200.css';
+	import '@fontsource/iosevka-etoile/200-italic.css';
+	import '@fontsource/iosevka-etoile/300.css';
+	import '@fontsource/iosevka-etoile/300-italic.css';
+	import '@fontsource/iosevka-etoile/400.css';
+	import '@fontsource/iosevka-etoile/400-italic.css';
+	import '@fontsource/iosevka-etoile/500.css';
+	import '@fontsource/iosevka-etoile/500-italic.css';
+	import '@fontsource/iosevka-etoile/600.css';
+	import '@fontsource/iosevka-etoile/600-italic.css';
+	import '@fontsource/iosevka-etoile/700.css';
+	import '@fontsource/iosevka-etoile/700-italic.css';
+	import '@fontsource/iosevka-etoile/800.css';
+	import '@fontsource/iosevka-etoile/800-italic.css';
+	import '@fontsource/iosevka-etoile/900.css';
+	import '@fontsource/iosevka-etoile/900-italic.css';
+
 	import '../app.css';
 </script>
 
