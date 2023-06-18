82a5d553a19d3a9150d8e902f967f4f8d179b312
Bryon Sigawin <zh3nlong@gmail.com>
2023-06-18 16:28:04 +0800

    fix: month formatting

--- src/routes/dashboard/transactions.svelte
+++ src/routes/dashboard/transactions.svelte
@@ -18,7 +18,7 @@
 		const groups = structuredClone(_groups);
 
 		const date = dayjs(current.date);
-		const month = date.month() + 1;
+		const month = date.month();
 		const year = date.year();
 
 		const groupYear = groups[year];
@@ -40,7 +40,10 @@
 			<div>
 				<div class="mb-6 flex justify-end items-end gap-6">
 					<h2 class="text-2xl align-bottom mr-auto">
-						{dayjs(`${transactionYear}/${transactionMonth}/1`).format('MMM YYYY')}
+						{dayjs()
+							.year(parseInt(transactionYear))
+							.month(parseInt(transactionMonth))
+							.format('MMM YYYY')}
 					</h2>
 
 					<div class="text-right">
